ab06d21c271e4cb93276f9a4bf75abd1b698b1bb
Andre0n <andreon098@gmail.com>
2024-10-06 15:05:45 -0300

    refactor: reformat file using prettier

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,40 @@
-import app from "./app";
+import app from './app';
 import config from './config';
 
 const server = app.listen(config.port, config.host, () => {
   console.log(`Server running on ${config.baseUrl}`);
 });
 
-server.on("unhandledRejection", (err: Error) => {
-  console.log("Unhandled Rejection! Shutting down server...");
+server.on('unhandledRejection', (err: Error) => {
+  console.log('Unhandled Rejection! Shutting down server...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
 
-server.on("error", (err: Error) => {
-  console.log("Unhandled Error! Shutting down server...");
+server.on('error', (err: Error) => {
+  console.log('Unhandled Error! Shutting down server...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
 
-server.on("close", () => {
-  console.log("Server closed");
+server.on('close', () => {
+  console.log('Server closed');
 });
 
-process.on("uncaughtException", (err: Error) => {
-  console.log("Uncaught Exception! Shutting down server...");
+process.on('uncaughtException', (err: Error) => {
+  console.log('Uncaught Exception! Shutting down server...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
 
-process.on("SIGTERM", () => {
-  console.log("Shutting down server");
+process.on('SIGTERM', () => {
+  console.log('Shutting down server');
   server.close();
   process.exit();
 });
